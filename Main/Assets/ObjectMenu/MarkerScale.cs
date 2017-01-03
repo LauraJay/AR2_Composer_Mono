f82a3aa009036e5d8e286f602043c253fe2c9b78
zwubel <zwubel@gmail.com>
2017-01-03 14:51:03 +0100

    Necessary commit to switch to XMG.

--- Main/Assets/ObjectMenu/MarkerScale.cs
+++ Main/Assets/ObjectMenu/MarkerScale.cs
@@ -13,7 +13,7 @@ public class MarkerScale : MonoBehaviour {
     private Vector3 newScale;
 
     // Use this for initialization
-    void Start (){
+    void Start() {
         xHandle = gameObject.transform.parent.FindChild("X_Handle");
         originalPosXY.x = xHandle.localPosition.x;
         yHandle = gameObject.transform.parent.FindChild("Y_Handle");
@@ -27,18 +27,15 @@ public class MarkerScale : MonoBehaviour {
 	// Update is called once per frame
 	void Update (){
         newScale.x = - xHandle.localPosition.x + originalPosXY.x + 1.0f;
-        if (newScale.x < originalScale.x)
-        {
+        if (newScale.x < originalScale.x){
             newScale.x = originalScale.x;
             xHandle.localPosition = new Vector3(originalPosXY.x, xHandle.localPosition.y, xHandle.localPosition.z);
         }
         newScale.y = - yHandle.localPosition.z + originalPosXY.y + 1.0f;
-        if (newScale.y < originalScale.y)
-        {
+        if (newScale.y < originalScale.y){
             newScale.y = originalScale.y;
             yHandle.position = new Vector3(yHandle.localPosition.x, yHandle.localPosition.y, originalPosXY.y);
         }
-        Debug.Log(newScale);
         gameObject.transform.localScale = new Vector3(newScale.x, gameObject.transform.localScale.y, newScale.y);
         zHandle.localPosition = new Vector3(originalPosZ.x - newScale.x + 1.0f, zHandle.localPosition.y, originalPosZ.y - newScale.y + 1.0f);
     }
